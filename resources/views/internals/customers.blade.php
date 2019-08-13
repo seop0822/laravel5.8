90fbb6d79b3d0d0480cedd36453ef18404bac25f
unknown <seop0822@naver.com>
2019-08-13 14:03:04 +0900

    Eloquent Where Caluse

--- resources/views/internals/customers.blade.php
+++ resources/views/internals/customers.blade.php
@@ -26,16 +26,35 @@
                     <input type="text" name="email" value="{{ old('email') }}" class="form-control">
                     <div>{{ $errors->first('email') }}</div>
                 </div>
+
+                <div class="form-group">
+                    <label for="active">Stauts </label>
+                    <select name="active" id="active" class="form-control">
+                        <option value="" disabled>Select cusotmer status</option>
+                        <option value="1">Active</option>
+                        <option value="0">Inactive</option>
+                    </select>
+                </div>
                 <button type="submit" class="btn btn-primary">Add Customer</button>
             </form>
         </div>
     </div>
 
     <div class="row">
-        <div class="col-12">
+        <div class="col-6">
+            <h3>Active Customers</h3>
             <ul>
-                @foreach ($customers as $customer)
-                    <li>{{ $customer->name }} <span class="text-muted">{{ $customer->email }}</span></li>
+                @foreach ($activeCustomers as $activeCustomer)
+                    <li>{{ $activeCustomer->name }} <span class="text-muted">{{ $activeCustomer->email }}</span></li>
+                @endforeach
+            </ul>
+        </div>
+
+        <div class="col-6">
+            <h3>Inactive Customers</h3>
+            <ul>
+                @foreach ($inactiveCustomers as $inactiveCustomer)
+                    <li>{{ $inactiveCustomer->name }} <span class="text-muted">{{ $inactiveCustomer->email }}</span></li>
                 @endforeach
             </ul>
         </div>
