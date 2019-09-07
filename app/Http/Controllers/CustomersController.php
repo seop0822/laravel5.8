ac21df2460deb1e9edf7d98f24d66c72aacbf9aa
unknown <seop0822@naver.com>
2019-09-08 02:16:02 +0900

    Artisan Authentication Restricting Access with Middleware

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -9,6 +9,11 @@ use Illuminate\Http\Request;
 
 class CustomersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index']);
+    }
+
     public function index()
     {
         $activeCustomers = Customer::active()->get();
